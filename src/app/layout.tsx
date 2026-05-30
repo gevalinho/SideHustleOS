535c2fecc5188df8ed1d0ca88651bc788b109969
gevalinho <gevalinho1@gmail.com>
2026-05-30 09:44:55 +0100

    SideHustleOs initial setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   )
 }
